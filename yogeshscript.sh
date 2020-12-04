9f5625eb9f39d90595774aac8677a25979b93749
root <root@ansiblemaster.us-central1-a.c.thinknyxukproject.internal>
2020-12-04 09:44:41 +0000

    test script

---
+++ yogeshscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Starting my second project for GIT and Jenkins test"
+echo "###################################################"
+yum -y install telnet
+mkdir /var/tmp/yogeshtest
+echo "Script compelted with success"
